83a5a74fbf9326ac298bc3b07b7f098d8f8aa552
won-jae-jang <dnjswo410@naver.com>
2024-04-08 14:27:46 +0900

    04-08 자바 예외 계층 연습

---
+++ src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -0,0 +1,59 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(service::callThrow)
+                .isInstanceOf(MyUncheckedException.class);
+    }
+
+    /**
+     * RuntimeException을 상속받은 예외는 언체크 예외가 된다
+     */
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try{
+               repository.call();
+            } catch (MyUncheckedException e){
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /**
+         * 예외를 잡지 않아도 된다. 자연스럽게 상위로 넘어간다
+         * 체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.
+         */
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+}
